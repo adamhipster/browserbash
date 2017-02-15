c1065365b03c07ff40fe434ef9078adfc5303ddd
melvinroest <melvinroest@gmail.com>
2017-02-15 20:45:11 +0100

    command line in browser is working with express. I also formatted the code in a way that I understand

--- server.js
+++ server.js
@@ -1,34 +1,7 @@
-//
-// This server will start a bash shell and expose it
-// over socket.io to a browser. See ./term.html for the 
-// client side.
-// 
-// You should probably:
-//
-//   npm install socket.io
-//   curl -O https://github.com/LearnBoost/Socket.IO/raw/master/socket.io.min.js
-//
-// To get socket.io in the node_modules directory and
-// the socket.io.min.js file needed for the client.
-//
-// To start the server:
-//
-//   node server.js
-//
-// And then load up your term!
-//
-//   open http://`hostname`:8080/term.html
-//
-// You can even share the url with a friend on your
-// local network. Be sure they're a friend though :-)
-//
-
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
-const sys   = require('sys');
-const util  = require('util');
 const spawn = require('child_process').spawn;
 const sh = spawn('bash');
 
@@ -46,12 +19,9 @@ sh.on('exit', function (code) {
   io.emit('exit', '** Shell exited: '+code+' **');
 });
 
-
 io.on('connection', function(client){
   client.on('message', function(data){
     sh.stdin.write(data+"\n");
-    console.log('data')
-    console.log(data)
     io.emit('message', new Buffer("> "+data));
   });
 });
